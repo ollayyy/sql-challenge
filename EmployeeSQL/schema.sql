5892d7f5364ee8bb256247894cf1bdd4aefdb127
Oliver <ollayeve@gmail.com>
2023-05-03 13:41:13 -0500

    Schema/ERD

---
+++ EmployeeSQL/schema.sql
@@ -0,0 +1,64 @@
+CREATE TABLE dept_manager(
+	dept_no VARCHAR(10) NOT NULL,
+	emp_no INT NOT NULL,
+	FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(10) NOT NULL,
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY(dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE employees(
+	emp_no INT NOT NULL PRIMARY KEY,
+	emp_title_id VARCHAR(10) NOT NULL,
+	birth_date date NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	sex VARCHAR(2) NOT NULL,
+	hire_date date NOT NULL,
+	FOREIGN KEY(emp_title_id) REFERENCES titles(title_id)
+);
+
+CREATE TABLE titles(
+	title_id VARCHAR(10) NOT NULL PRIMARY KEY,
+	tile VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE departments(
+	dept_no VARCHAR(10) NOT NULL PRIMARY KEY,
+	dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE salaries(
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
